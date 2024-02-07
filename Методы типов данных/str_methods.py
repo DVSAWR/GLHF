f1e23f84886f575f88361f3d4acddc8c39983b53
DVSAWR <126908465+DVSAWR@users.noreply.github.com>
2024-02-07 17:25:18 +0300

    add

--- Методы типов данных/str_methods.py
+++ Методы типов данных/str_methods.py
@@ -6,19 +6,19 @@ x = 'Что ТАКОЕ LOREM Ipsum?'
 print(f'str = {x}')
 
 preview('capitalize()')
-
-print(x.capitalize())
 # Переводит первый символ/букву строки в верхний регистр
+print(x.capitalize())
+
 preview('casefold()')
-
-print(x.casefold())
 # Сворачивает регистр строки
+print(x.casefold())
+
 preview('center()')
-
-print(x.center(len(x)+20, '_'))
 # Производит выравнивание текста в строке по центру
-preview('count()')
+print(x.center(len(x) + 20, '_'))
 
+preview('count()')
+# Подсчитывает количество вхождений символа/подстроки в строку
 print(x.count('O'))
 print(x.count('O', 2))
 print(x.count('O', 2, 5))
@@ -158,7 +158,7 @@ print(z)
 
 preview('ljust()')
 # Выравнивает текст в строке по левому краю
-print(x.ljust(len(x)+20, '_'))
+print(x.ljust(len(x) + 20, '_'))
 
 preview('lower()')
 # Переводит строку в нижний регистр
@@ -189,7 +189,130 @@ t = line.maketrans(y, z)
 print(t)
 print(line.translate(t))
 
-preview('')
+preview('partition()')
+# Производит деление строки по первому совпадению символа/подстроки
+print(f'{x} = {x.partition(" ")}')
+print(f'{x} = {x.partition("ТАКОЕ")}')
 
+preview('removeprefix()')
+# Производит удаление префикса строки, если таковой имеется
+y = 'LoremIpsum'
+print(f'{y} = {y.removeprefix("Lorem")}')
+print(f'{y} = {y.removeprefix("Ipsum")}')
 
+preview('removesuffix()')
+# Производит удаление суффикса строки, если таковой имеется
+y = 'LoremIpsum'
+print(f'{y} = {y.removesuffix("Lorem")}')
+print(f'{y} = {y.removesuffix("Ipsum")}')
 
+preview('replace()')
+# Производит поиск и замену подстроки (символа) в строке
+print(f'{x} = {x.replace(" ", "*")}')
+print(f'{x} = {x.replace(" ", "*", 2)}')
+
+preview('rfind()')
+# Возвращает наибольший индекс совпадения символа/подстроки (индекс последнего совпадения)
+y = 'qwe qwe qwerty'
+print(f'{y} = {y.rfind("qwe")}')
+print(f'{y} = {y.rfind("asd")}')
+
+preview('rindex()')
+# Возвращает наибольший индекс совпадения символа/подстроки
+y = 'qwe qwe qwerty'
+print(f'{y} = {y.rindex("qwe")}')
+
+preview('rjust()')
+# Выравнивает текст в строке по правому краю
+print(x.rjust(len(x) + 20, '_'))
+
+preview('rpartition()')
+# Разделить строку по последнему совпадению символа/подстроки в строке
+y = 'qwe qwe qwerty'
+print(f'{y} = {y.rpartition("qwe")}')
+
+preview('rsplit()')
+# Разбивает строку по символу/подстроке начиная справа
+print(f'{x} = {x.rsplit(" ")}')
+print(f'{x} = {x.rsplit(" ", 2)}')
+
+preview('rstrip()')
+# Удаляет символы в конце строки
+print(x.rstrip())
+print(x.rstrip('Ipsum?'))
+print(x.rstrip('LOREM Ipsum?'))
+
+preview('split()')
+# Разделить строку на список подстрок по разделителю
+print(f'{x} = {x.split()}')
+print(f'{x} = {x.split("LOREM")}')
+
+preview('splitlines()')
+# Делит текст на список строк по разрывам строк
+y = 'Что ТАКОЕ\n LOREM\r Ipsum?'
+print(f'{y} = {y.splitlines()}')
+print(f'{y} = {y.splitlines(keepends=True)}')
+
+preview('startswith()')
+# Возвращает True, если строка str начинается указанным префиксом
+print(f'{x} = {x.startswith("Что")}')
+print(f'{x} = {x.startswith("ТАКОЕ")}')
+
+print('')
+y = ['Что ТАКОЕ LOREM Ipsum?',
+     'ТАКОЕ LOREM Ipsum?',
+     'LOREM Ipsum?']
+prefix = ('Что', 'LOREM')
+print(prefix)
+
+for i in y:
+    if i.startswith(prefix):
+        print(i, '= YES')
+    else:
+        print(i, '= NO')
+
+preview('strip()')
+# Удаляет начальные и конечные символы в строке
+y = '___qwe qwerty qwe___'
+print(y)
+print(y.strip('_'))
+
+print('')
+y = 'www.example.com'
+print(y)
+print(y.strip('cmowz.'))
+
+preview('swapcase()')
+# Сменить регистр символов в строке
+print(f'{x} = {x.swapcase()}')
+
+preview('title()')
+# Переводит первую букву каждого слова в строке в верхний регистр
+print(f'{x} = {x.title()}')
+
+preview('translate()')
+# Транслировать текст по определенной схеме str.maketrans()
+y = {'q': '0', 'w': '1', 'e': '2'}
+line = 'qwerty qwe rty'
+print(line)
+t = line.maketrans(y)
+print(t)
+print(line.translate(t))
+
+y = 'qwe'
+z = '012'
+line = 'qwerty qwe rty'
+print('')
+print(line)
+t = line.maketrans(y, z)
+print(t)
+print(line.translate(t))
+
+preview('upper()')
+# Преобразовать строку в верхний регистр
+print(f'{x} = {x.upper()}')
+
+preview('zfill()')
+# Заполняет начало строки нулями
+y = '123'
+print(f'{y} = {y.zfill(5)}')
