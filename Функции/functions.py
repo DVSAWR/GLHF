a1a8f841fc27137a798f20625d98961c4d78d003
DVSAWR <126908465+DVSAWR@users.noreply.github.com>
2024-02-09 17:14:33 +0300

    add

--- Функции/functions.py
+++ Функции/functions.py
@@ -32,11 +32,79 @@ num2 = range(0, 15, 2)
 print(any([x > 15 for x in num1]))
 print(any([x > 15 for x in num2]))
 
-
 preview('ascii()')
 line = 'My name is Даниил'
 x = ascii(line)
 print(x)
 
+preview('bin()')
+# Преобразует целое число в двоичную строку
+print('bin(123)=', bin(123))
+
+preview('bool()')
+# Позволяет проверить/узнать логическое значение объекта
+print('bool(1) =', bool(1))
+print('bool(0) =', bool(0))
+
+preview('breakpoint()')
+# Обозначает точку останова и выполняет функцию отладчика кода
+print("x = 10 y = 'Hi' z = 'Hello' print(y) breakpoint() print(z) "
+      "Когда мы запустим этот скрипт, встретив точку останова в коде "
+      "интерпретатор затормозит процесс исполнения, открыв консоль "
+      "отладчика PDB, что позволит нам детально разобраться в "
+      "происходящем. Чтобы прервать PDB-сессию и продолжить выполнение "
+      "скрипта, нажмите C и Enter.")
+
+preview('bytearray()')
+# Преобразует объект в изменяемый байтовый массив
+print('bytearray(4) =', bytearray(4))
+print('bytearray("Привет", encoding="utf-8" =', bytearray("Привет", encoding="utf-8"))
+
+preview('bytes()')
+# Преобразует объект в неизменяемую строку байтов
+print('bytes(4) =', bytes(4))
+print('bytes("Привет", encoding="utf-8" =', bytes("Привет", encoding="utf-8"))
+
+preview('callable()')
 
 
+# Проверяет, является ли объект вызываемым
+class MyClass:
+    def __call__(self, *args, **kwargs):
+        print('called')
+
+
+print('callable(MyClass()) =', callable(MyClass()))
+
+print('callable(5) =', callable(5))
+
+preview('chr()')
+# Преобразует число в символ Юникода, обратная операция ord()
+print("ord('q') =", ord('q'))
+print("chr(113) =", chr(113))
+
+lst = []
+for i in range(49, 58):
+    lst.append(chr(i))
+print(lst)
+
+preview('classmethod()')
+
+
+# Позволяет преобразовать обычный метод в метод класса
+class MyClassMethod():
+
+    @classmethod
+    def method(cls, arg):
+        print(f'{cls.__name__}.method = {arg}')
+
+
+print('class :')
+MyClassMethod.method('qwe')
+
+mcm_obj = MyClassMethod()
+print('\nobject:')
+mcm_obj.method('zxc')
+
+preview('compile()')
+# Позволяет скомпилировать блок кода для выполнения в exec()
