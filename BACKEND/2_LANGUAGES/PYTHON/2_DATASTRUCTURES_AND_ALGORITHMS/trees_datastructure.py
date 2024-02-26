b79a5f7e8444d3141d6c9692778e30c12214ee22
Daniil Gruzdev <gruzdev.daniil@gmail.com>
2024-02-26 16:04:43 +0300

    add

--- BACKEND/2_LANGUAGES/PYTHON/2_DATASTRUCTURES_AND_ALGORITHMS/trees_datastructure.py
+++ BACKEND/2_LANGUAGES/PYTHON/2_DATASTRUCTURES_AND_ALGORITHMS/trees_datastructure.py
@@ -1,2 +1,213 @@
 def preview(x):
     print(f'\n---- {x} ----')
+
+preview('TREES')
+print()
+
+
+print()
+preview('BINARY TREE')
+
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    # Insert Node
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+            else:
+                self.data = data
+
+    # Print tree
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data, end=' ')
+        if self.right:
+            self.right.print_tree()
+
+
+    # Preorder traversal
+    def traversal(self, root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.traversal(root.left)
+            res = res + self.traversal(root.right)
+        return res
+
+
+root = Node(27)
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+print(root.traversal(root))
+
+root.print_tree()
+print()
+
+print()
+preview('BINARY SEARCH TREE')
+print()
+
+class BSTNode:
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
+
+
+    def get_min(self):
+        current = self
+        while current.left is not None:
+            current = current.left
+        return current.val
+
+
+    def get_max(self):
+        current = self
+        while current.right is not None:
+            current = current.right
+        return current.val
+
+
+    def delete(self, val):
+        if self == None:
+            return self
+        if self.right == None:
+            return self.left
+        if self.left == None:
+            return self.right
+        if val < self.val:
+            if self.left:
+                self.left = self.left.delete(val)
+            return self
+        if val > self.val:
+            if self.right:
+                self.right = self.right.delete(val)
+            return self
+        min_larger_node = self.right
+        while min_larger_node.left:
+            min_larger_node = min_larger_node.left
+        self.val = min_larger_node.val
+        self.right = self.right.delete(min_larger_node.val)
+        return self
+
+    def exists(self, val):
+        if val == self.val:
+            return True
+
+        if val < self.val:
+            if self.left == None:
+                return False
+            return self.left.exists(val)
+
+        if self.right == None:
+            return False
+        return self.right.exists(val)
+
+
+    def preorder(self, vals):
+        if self.val is not None:
+            vals.append(self.val)
+        if self.left is not None:
+            self.left.preorder(vals)
+        if self.right is not None:
+            self.right.preorder(vals)
+        return vals
+
+    def inorder(self, vals):
+        if self.left is not None:
+            self.left.inorder(vals)
+        if self.val is not None:
+            vals.append(self.val)
+        if self.right is not None:
+            self.right.inorder(vals)
+        return vals
+
+    def postorder(self, vals):
+        if self.left is not None:
+            self.left.postorder(vals)
+        if self.right is not None:
+            self.right.postorder(vals)
+        if self.val is not None:
+            vals.append(self.val)
+        return vals
+
+
+
+def main():
+    nums = [12, 6, 18, 19, 21, 11, 3, 5, 4, 24, 18]
+    bst = BSTNode()
+    for num in nums:
+        bst.insert(num)
+    print("preorder:")
+    print(bst.preorder([]))
+    print("#")
+
+    print("postorder:")
+    print(bst.postorder([]))
+    print("#")
+
+    print("inorder:")
+    print(bst.inorder([]))
+    print("#")
+
+    nums = [2, 6, 20]
+    print("deleting " + str(nums))
+    for num in nums:
+        bst.delete(num)
+    print("#")
+
+    print("4 exists:")
+    print(bst.exists(4))
+    print("2 exists:")
+    print(bst.exists(2))
+    print("12 exists:")
+    print(bst.exists(12))
+    print("19 exists:")
+    print(bst.exists(19))
+
+main()
+
+
+
+
+
