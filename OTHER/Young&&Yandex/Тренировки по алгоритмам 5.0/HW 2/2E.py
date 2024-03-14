15f5f2a065047422ab4daae5a695960202d8a640
DVSAWR <126908465+DVSAWR@users.noreply.github.com>
2024-03-15 00:40:23 +0300

    add

--- OTHER/Young&&Yandex/Тренировки по алгоритмам 5.0/HW 2/2E.py
+++ OTHER/Young&&Yandex/Тренировки по алгоритмам 5.0/HW 2/2E.py
@@ -97,185 +97,104 @@ i
 
 '''
 
-print('-------TRY 1-------\n')
+#
+# print('\n-------------TRY-------------\n')
 
-# print('# INPUT')
+# # INPUT
 #
 # n = int(input())
-# berry = {}
+# berry = []
 #
 # for i in range(n):
+#     position = i + 1
 #     b, d = map(int, input().split())
-#     berry[i + 1] = b - d
+#     value = b - d
+#     berry.append((position, b, d, value))
 #
 # print(berry)
-
-
-n = 3
-
-# berry1 = {1: -4, 2: 6, 3: 0}
-
-berry1 = {1: 1, 2: 3}
-
-sorted_berry = dict(sorted(berry1.items(), key=lambda item: item[1], reverse=True))
-
-print(berry1)
-print(sorted_berry)
-
-max_height = 0
-numberring = []
-
-for k, v in sorted_berry.items():
-    if v > 0:
-        max_height += v
-        numberring.append(k)
-    else:
-        numberring.append(k)
-
-print('\nANSWER: ', end=' ')
-print(max_height)
-print('SEQ: ', end=' ')
-print(numberring)
-
-print('\n-------------TRY 2-------------\n')
-
-# INPUT
-
-n = int(input())
-berry = []
-
-for i in range(n):
-    position = i + 1
-    b, d = map(int, input().split())
-    value = b - d
-    berry.append((position, b, d, value))
-
-print(berry)
-
-sorted_berry_value = sorted(berry, key=lambda item: item[3], reverse=True)
-
-print('\nSORT BY VALUE:', sorted_berry_value)
-
-# n = 6
 #
-# berry = [(1, 1, 1, 0), (2, 4, 4, 0), (3, 100, 200, -100), (4, 0, 50, -50), (5, 50, 1, 49), (6, 1, 50, -49)]
+# sorted_berry_value = sorted(berry, key=lambda item: item[3], reverse=True)
 #
-# sorted_berry_value = [(5, 50, 1, 49), (1, 1, 1, 0), (2, 4, 4, 0), (6, 1, 50, -49), (4, 0, 50, -50), (3, 100, 200, -100)]
+# print('\nSORT BY VALUE:', sorted_berry_value)
+#
+# # n = 6
+# #
+# # berry = [(1, 1, 1, 0), (2, 4, 4, 0), (3, 100, 200, -100), (4, 0, 50, -50), (5, 50, 1, 49), (6, 1, 50, -49)]
+# #
+#
+# # n = 6
+# # sorted_berry_value = [(5, 50, 1, 49), (1, 1, 1, 0), (2, 4, 4, 0), (6, 1, 50, -49), (4, 0, 50, -50), (3, 100, 200, -100)]
+#
+#
+# max_buff_berry = 0
+# max_buff_berry_index = None
+#
+# for i in range(n):
+#     if sorted_berry_value[i][1] > max_buff_berry:
+#         max_buff_berry = sorted_berry_value[i][1]
+#         max_buff_berry_index = i
+#         print(max_buff_berry)
+#
+# max_buff_berry = sorted_berry_value[max_buff_berry_index]
+#
+# print('\nMAX BERRY:', max_buff_berry, '\nMAX BERRY INDEX:', max_buff_berry_index)
+#
+# # 0 - b index
+# # 1 - b buff
+# # 2 - b debuff
+# # 3 - b value
+#
+# maximum_position = 0
+# current_position = 0
+#
+# find_max_buff_berry = 0
+#
+# berry_sequence = []
+#
+# for i in range(n):
+#
+#     if sorted_berry_value[i][3] > 0:
+#
+#         current_position += sorted_berry_value[i][1]
+#         if current_position > maximum_position:
+#             maximum_position = current_position
+#         current_position -= sorted_berry_value[i][2]
+#
+#         if i == max_buff_berry:
+#             find_max_buff_berry = 1
+#
+#         berry_sequence.append(sorted_berry_value[i][0])
+#
+#     else:
+#         if find_max_buff_berry:
+#             berry_sequence.append(sorted_berry_value[i][0])
+#         else:
+#             berry_sequence.append(sorted_berry_value[max_buff_berry_index][0])
+#
+#             maximum_position = current_position + sorted_berry_value[max_buff_berry_index][1]
+#
+#             if n > 2:
+#                 tail = sorted_berry_value[i:max_buff_berry_index] + sorted_berry_value[max_buff_berry_index + 1:]
+#                 print('\nTAIL:', tail)
+#                 for i in range(len(tail)):
+#                     berry_sequence.append(tail[i][0])
+#
+#             break
+#
+# print(f'\n\nMAX POS: {maximum_position}\nBERRY SEQ: {berry_sequence}')
+#
+# print('\n')
+#
+#
+# print('\n--------------DONE---------\n')
+#
+# print(maximum_position)
+# print(' '.join(map(str, berry_sequence)))
 
 
-max_buff_berry = None
-max_buff_berry_index = None
-
-for i in range(n):
-    if sorted_berry_value[i][1] > i:
-        max_buff_berry = sorted_berry_value[i]
-        max_buff_berry_index = i
-
-print('\nSORTED LIST:', sorted_berry_value)
-print('MAX BERRY:', max_buff_berry, 'MAX BERRY INDEX:', max_buff_berry_index)
-
-# 0 - b index
-# 1 - b buff
-# 2 - b debuff
-# 3 - b value
-
-maximum_position = 0
-current_position = 0
-
-find_max_buff_berry = 0
-
-berry_sequence = []
-
-for i in range(n):
-
-    if sorted_berry_value[i][3] > 0:
-
-        current_position += sorted_berry_value[i][1]
-        if current_position > maximum_position:
-            maximum_position = current_position
-        current_position -= sorted_berry_value[i][2]
-
-        if i == max_buff_berry:
-            find_max_buff_berry = 1
-
-        berry_sequence.append(sorted_berry_value[i][0])
-
-    else:
-        if find_max_buff_berry:
-            berry_sequence.append(sorted_berry_value[i][0])
-        else:
-            berry_sequence.append(sorted_berry_value[max_buff_berry_index][0])
-
-            maximum_position = current_position + sorted_berry_value[max_buff_berry_index][1]
-
-            if n > 2:
-                tail = sorted_berry_value[i:max_buff_berry_index] + sorted_berry_value[max_buff_berry_index + 1:]
-                print('\nTAIL:', tail)
-                for i in range(len(tail)):
-                    berry_sequence.append(tail[i][0])
-
-            break
-
-print(f'\n\nMAX POS: {maximum_position}\nBERRY SEQ: {berry_sequence}')
-
-print('\n')
-
-print(maximum_position)
-print(' '.join(map(str, berry_sequence)))
-
-print('\n--------------DONE---------\n')
-
-berry = []
-berry_sequence = []
-maximum_position = 0
-current_position = 0
-find_max_buff_berry = 0
-max_buff_berry = None
-max_buff_berry_index = None
-n = int(input())
-
-for i in range(n):
-    position = i + 1
-    b, d = map(int, input().split())
-    value = b - d
-    berry.append((position, b, d, value))
-sorted_berry_value = sorted(berry, key=lambda item: item[3], reverse=True)
-
-for i in range(n):
-    if sorted_berry_value[i][1] > i:
-        max_buff_berry = sorted_berry_value[i]
-        max_buff_berry_index = i
-
-for i in range(n):
-    if sorted_berry_value[i][3] > 0:
-
-        current_position += sorted_berry_value[i][1]
-        if current_position > maximum_position:
-            maximum_position = current_position
-        current_position -= sorted_berry_value[i][2]
-
-        if i == max_buff_berry:
-            find_max_buff_berry = 1
-
-        berry_sequence.append(sorted_berry_value[i][0])
-
-    else:
-        if find_max_buff_berry:
-            berry_sequence.append(sorted_berry_value[i][0])
-        else:
-            berry_sequence.append(sorted_berry_value[max_buff_berry_index][0])
-            maximum_position = current_position + sorted_berry_value[max_buff_berry_index][1]
-
-            if n > 2:
-                tail = sorted_berry_value[i:max_buff_berry_index] + sorted_berry_value[max_buff_berry_index + 1:]
-                for i in range(len(tail)):
-                    berry_sequence.append(tail[i][0])
-
-            break
-
-print(maximum_position)
-print(' '.join(map(str, berry_sequence)))
-
+# 822889311 446755913 480845266 448565595 715477619 181424399
+#
+# 534053220 32279671 822889311
 
 # 7
 # 160714711 449656269
@@ -285,3 +204,127 @@ print(' '.join(map(str, berry_sequence)))
 # 561330066 605997004
 # 61020590 573085537
 # 715477619 181424399
+
+# 1503796355
+# 2 4 7 5 1 3 6
+
+
+print('\n--------xxxxxxxxx-------\n')
+
+# n = 7
+# berry = [(7, 715477619, 181424399, 534053220), (2, 822889311, 446755913, 376133398),
+#          (4, 480845266, 448565595, 32279671), (5, 561330066, 605997004000, -605435673934),
+#          (3, 135599877, 389312924, -253713047), (1, 160714711, 449656269, -288941558),
+#          (6, 61020590, 573085537, -512064947)]
+# 1503796355
+# 2 4 7 5 1 3 6
+
+
+n = int(input())
+
+left = []
+right = []
+
+nleft = 0
+nright = 0
+
+
+for i in range(n):
+    position = i + 1
+    b, d = map(int, input().split())
+    value = b - d
+    if value > 0:
+        left.append((position, b, d, value))
+        nleft += 1
+
+    else:
+        right.append((position, b, d, value))
+        nright += 1
+
+# n = 7
+# left = [(2, 822889311, 446755913, 376133398), (4, 480845266, 448565595, 32279671), (7, 715477619, 181424399, 534053220)]
+# right = [(1, 160714711, 449656269, -288941558), (3, 135599877, 389312924, -253713047), (5, 561330066, 605997004, -44666938), (6, 61020590, 573085537, -512064947)]
+
+
+print('LEFT:\n', left)
+print(f'LEN: {nleft}')
+print('RIGHT:\n', right)
+print(f'LEN: {nright}')
+
+sorted_left = sorted(left, key=lambda item: item[3], reverse=True)
+sorted_right = sorted(right, key=lambda item: item[1], reverse=True)
+
+print(f'\nSORTED LEFT:\n{sorted_left}\n\nSORTED RIGHT:\n{sorted_right}')
+
+maximum_position = 0
+current_position = 0
+berry_seq = []
+
+for i in range(nleft):
+    current_position += sorted_left[i][1]
+    if current_position > maximum_position:
+        maximum_position = current_position
+    current_position -= sorted_left[i][2]
+    berry_seq.append(sorted_left[i][0])
+
+if nright != 0:
+    if maximum_position < current_position + sorted_right[0][1]:
+        maximum_position = current_position + sorted_right[0][1]
+
+    for i in sorted_right:
+        berry_seq.append(i[0])
+
+print(f'\nMAX POS: {maximum_position}\nCUR POS: {current_position}')
+print(berry_seq)
+
+print('\n-------TEST--------\n')
+
+
+n = int(input())
+
+left = []
+right = []
+
+nleft = 0
+nright = 0
+
+for i in range(n):
+    position = i + 1
+    b, d = map(int, input().split())
+    value = b - d
+    if value > 0:
+        left.append((position, b, d, value))
+        nleft += 1
+
+    else:
+        right.append((position, b, d, value))
+        nright += 1
+
+sorted_left = sorted(left, key=lambda item: item[3], reverse=True)
+sorted_right = sorted(right, key=lambda item: item[1], reverse=True)
+
+maximum_position = 0
+current_position = 0
+berry_seq = []
+
+for i in range(nleft):
+    current_position += sorted_left[i][1]
+    if current_position > maximum_position:
+        maximum_position = current_position
+    current_position -= sorted_left[i][2]
+    berry_seq.append(sorted_left[i][0])
+
+if nright != 0:
+    if maximum_position < current_position + sorted_right[0][1]:
+        maximum_position = current_position + sorted_right[0][1]
+
+    for i in sorted_right:
+        berry_seq.append(i[0])
+
+print(maximum_position)
+print(' '.join(map(str, berry_seq)))
+
+
+
+# TEST
+
